66c75f2481ac33975da75f68b1be53d54e80f0e9
ssumaiyah <ssumaiyah@academic.rrc.ca>
2024-07-22 15:06:32 -0500

    added category option on the product page of admin

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,9 +1,8 @@
-# app/admin/product.rb
+# app/admin/products.rb
 
 ActiveAdmin.register Product do
-
   filter :image_attachment_id
-  permit_params :name, :description, :price, :quantity_available, :artisan_id, :image
+  permit_params :name, :description, :price, :quantity_available, :artisan_id, :image, category_ids: [], new_category_name: nil
 
   form do |f|
     f.semantic_errors *f.object.errors.full_messages
@@ -17,7 +16,36 @@ ActiveAdmin.register Product do
       f.input :image, as: :file, hint: f.object.image.attached? ? image_tag(f.object.image.variant(resize_to_limit: [100, 100])) : content_tag(:span, "No image yet")
     end
 
+    f.inputs 'Categories' do
+      f.input :category_ids, as: :check_boxes, collection: Category.all.map { |c| [c.name, c.id] }
+      f.input :new_category_name, as: :string, input_html: { value: params.dig(:product, :new_category_name) }, label: 'Add New Category'
+    end
+
     f.actions
+  end
+
+  controller do
+    def create
+      new_category_name = params.dig(:product, :new_category_name)
+      if new_category_name.present?
+        new_category = Category.find_or_create_by(name: new_category_name)
+        params[:product][:category_ids] ||= []
+        params[:product][:category_ids] << new_category.id unless params[:product][:category_ids].include?(new_category.id.to_s)
+      end
+
+      super
+    end
+
+    def update
+      new_category_name = params.dig(:product, :new_category_name)
+      if new_category_name.present?
+        new_category = Category.find_or_create_by(name: new_category_name)
+        params[:product][:category_ids] ||= []
+        params[:product][:category_ids] << new_category.id unless params[:product][:category_ids].include?(new_category.id.to_s)
+      end
+
+      super
+    end
   end
 
   show do
@@ -29,6 +57,9 @@ ActiveAdmin.register Product do
       row :artisan
       row :image do |product|
         image_tag product.image.variant(resize_to_limit: [100, 100]) if product.image.attached?
+      end
+      row :categories do |product|
+        product.categories.map(&:name).join(', ')
       end
     end
     active_admin_comments
@@ -44,6 +75,9 @@ ActiveAdmin.register Product do
     column :artisan
     column :image do |product|
       image_tag product.image.variant(resize_to_limit: [50, 50]) if product.image.attached?
+    end
+    column :categories do |product|
+      product.categories.map(&:name).join(', ')
     end
     actions
   end
