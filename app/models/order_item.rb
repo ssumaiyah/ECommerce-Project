616764097bb16b8fd69811ebf0fdc6eb9e3de24d
ssumaiyah <ssumaiyah@academic.rrc.ca>
2024-07-26 19:30:09 -0500

    fixed validation in order_item.rb

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,10 +3,10 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
-  validates :quantity, numericality: { only_integer: true, greater_than: 0 }
+ validates :quantity, numericality: { only_integer: true, greater_than: 0 }
   validates :order_id, presence: true
   validates :product_id, presence: true
-  validates :price_at_purchase, numericality: { greater_than_or_equal_to: 0 }
+
 
   def total_price
     product.price * quantity
