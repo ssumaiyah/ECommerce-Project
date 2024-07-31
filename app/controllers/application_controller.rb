cd1d22a61cfa45808477ae8bccd0606898a95a28
ssumaiyah <ssumaiyah@academic.rrc.ca>
2024-07-31 11:35:27 -0500

    now while signing up , users will write their name as well

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,8 +34,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:province_id])
     devise_parameter_sanitizer.permit(:account_update, keys: [:province_id])
+  
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
   end
-end
+
 
 
 
